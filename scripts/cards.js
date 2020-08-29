a093e8741b9cd718ffe758995afbe144d1da2b6a
AlekseiMakhov <66823049+AlekseiMakhov@users.noreply.github.com>
2020-08-29 23:18:17 +0300

    feat: добавить функционал 6 спринта

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,16 +1,13 @@
-const imageAddPopup = document.querySelector('#image'); //всплывающее окно формы добавления карточки
+const imageAddPopup = document.querySelector('#add-image'); //всплывающее окно формы добавления карточки
 const addForm = imageAddPopup.querySelector('.popup-form'); //форма добаления карточки
-const imageNameInput = addForm.querySelector('#image-name'); //поле ввода названия карточки
-const imageLinkInput = addForm.querySelector('#image-link'); //поле ввода ссылки
-const imageAddCloseButton = imageAddPopup.querySelector('.popup-form__close-button'); //кнопка закрытия
-const addPlaceButton = document.querySelector('.add-button'); //кнопка добавления карточки
+const imageNameInput = addForm.querySelector('#image-input'); //поле ввода названия карточки
+const imageLinkInput = addForm.querySelector('#link-input'); //поле ввода ссылки
+const addPlaceButton = document.querySelector('.add-image-button'); //кнопка добавления карточки
 const cardTempElement = document.querySelector('#place-card').content; //элемент с содержимым шаблона для карточки
-const imageViewPopup = document.querySelector('#image-view'); //всплывающее окно с увеличенным изображением
-const imageViewCloseButton = imageViewPopup.querySelector('.popup-image__close-button'); //кнопка закрытия окна
+const imageViewPopup = document.querySelector('#view-image'); //всплывающее окно с увеличенным изображением
 const imageViewElement = imageViewPopup.querySelector('.popup-image__image'); //увеличенное изображение
 const imageViewTitle = imageViewPopup.querySelector('.popup-image__title'); //подпись с названием
 const cardElements = document.querySelector('.elements'); //объект (секция с карточками)
-const cardTemp = cardTempElement.cloneNode(true); //клонированный template-тег карточки
 
 //функция открытия/скрытия окна формы добавления карточки
 const toggleAddImagePopup = () => {
@@ -18,22 +15,22 @@ const toggleAddImagePopup = () => {
 }
 
 //функция открытия/скрытия окна с увеличенным изображением
-const toggleImageViewPopup = () => {
-    imageViewPopup.classList.toggle('popup_opened');
+const openImageViewPopup = () => {
+    imageViewPopup.classList.add('popup_opened');
 }
 
 //Открытие окна с изображением
 const openImageView = (item) => {
     const elementTitle = item.parentElement.querySelector('.element__text');  
-    toggleImageViewPopup();
+    openImageViewPopup();
     imageViewElement.src = item.src;
     imageViewTitle.textContent = elementTitle.textContent;
     imageViewElement.alt = elementTitle.textContent + '. Фото';
 }
 
 //функция добавления элемента (карточки) в DOM
-const addElement = (item, elem) => {
-    const elem = cardTemp;
+const addElement = (item) => {
+    const elem = cardTempElement.cloneNode(true); //клонированный template-тег карточки
     const delButton = elem.querySelector('.element__trash-button'); //кнопка удаления карточки
     const image = elem.querySelector('.element__image'); //элемент карточки (изображение)
     const likeButton = elem.querySelector('.element__like'); //кнопка "лайк"
@@ -57,14 +54,17 @@ const addElement = (item, elem) => {
     elem.querySelector('.element__image').src = item.link;
     elem.querySelector('.element__image').alt = item.name + '. Фото';
     elem.querySelector('.element__text').textContent = item.name;
+    
+    //добавление карточки в DOM в первую позицию 
+    cardElements.prepend(elem);
 }
 
 //фунция заполнения формы карточки
 const addPlaceElement = evt => {
+    
     const item = {name: imageNameInput.value, link: imageLinkInput.value};
     evt.preventDefault();
-    addElement(item, cardTemp);
-    cardElements.prepend(cardTemp);
+    addElement(item);
     toggleAddImagePopup();   
 }
 
@@ -73,7 +73,5 @@ initialCards.forEach (item => {
     addElement(item);
 });
 
-imageAddCloseButton.addEventListener('click', toggleAddImagePopup); //Обработчик события для кнопки закрытия окна формы добавления карточки
 addPlaceButton.addEventListener('click', toggleAddImagePopup); //Обработчик события для кнопки добавления карточки
 addForm.addEventListener('submit', addPlaceElement); //Обработчик события для отправки формы добавления карточки
-imageViewCloseButton.addEventListener('click', toggleImageViewPopup); //Обработчик события для кнопки закрытия окна с изображением
--- scripts/profile.js
+++ scripts/profile.js
@@ -1,11 +1,11 @@
-const displayOverlay = document.querySelector('#profile'); //всплывающее окно формы редактирования профиля
+const displayOverlay = document.querySelector('#profile-info__edit'); //всплывающее окно формы редактирования профиля
 const name = document.querySelector('.profile-info__name'); //имя
 const description = document.querySelector('.profile-info__description'); //описание (профессия)
 const editForm = displayOverlay.querySelector('.popup-form'); //форма редактирования профиля
-const nameField = displayOverlay.querySelector('#profile-name'); //поле ввода имени
-const descField = displayOverlay.querySelector('#profile-description'); //поле ввода описания
+const nameField = displayOverlay.querySelector('#name-input'); //поле ввода имени
+const descField = displayOverlay.querySelector('#about-input'); //поле ввода описания
 const editButton = document.querySelector('.profile-info__edit-button'); //кнопка редактирования профиля
-const closeButton = displayOverlay.querySelector('.popup-form__close-button'); //кнопка закрытия окна
+
 
 //функция открытия/скрытия окна редактирования профиля
 const toggleInputFormDisplay = () => {
@@ -29,4 +29,3 @@ const submitEditing = evt => {
 
 editButton.addEventListener('click', editProfile); //обработчик события кнопки редактирования профиля
 editForm.addEventListener('submit', submitEditing); //обработчик события для отправки формы редактирования профиля
-closeButton.addEventListener('click', toggleInputFormDisplay); //обработчик события кнопки закрытия окна
